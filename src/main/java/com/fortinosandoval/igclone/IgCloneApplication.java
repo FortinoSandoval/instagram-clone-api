3abf204e7454ebe3f4e8b1436106ac587c92c895
Fortino Sandoval <fortino.sandoval@maritz.com>
2020-04-28 21:08:08 -0700

    initial commit

---
+++ src/main/java/com/fortinosandoval/igclone/IgCloneApplication.java
@@ -0,0 +1,13 @@
+package com.fortinosandoval.igclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortinosandoval/igclone/IgCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortinosandoval.igclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
